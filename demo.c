f3ba6bd25e3ddb2445eef48e14374a0b38105dfa
mouadse <mouadse@gmail.com>
2024-11-18 15:33:45 +0100

    X

--- demo.c
+++ demo.c
@@ -1,10 +1,20 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
+// ptr address
 // helpful function to upper
 int main(void) {
-  char *str = "hello world";
-  
+  char *ptr;
+  int age;
+
+  ptr = (char *)malloc(10);
+  // The adress it points to
+  printf("The address of ptr is: %p\n", ptr);
+  // The address of the pointer
+  printf("The address of &ptr is: %p\n", &ptr);
+  // The address of the variable
+  printf("The address of &age is: %p\n", &age);
   return (0);
 }
